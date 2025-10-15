535746848b1447287ace277caf5d2916a7059451
Ahmed <ahmeduae1233@yahoo.com>
2025-10-15 12:04:42 +0300

    adding model and env configs

--- src/model/model_builder.py
+++ src/model/model_builder.py
@@ -1,1 +1,6 @@
-from sklearn_crfsuite import CRF, metrics
+from sklearn_crfsuite import CRF
+from config import CRF_PARAMS
+
+def build_crf_model(args : dict) -> CRF:
+    model = CRF(**CRF_PARAMS)
+    return model
